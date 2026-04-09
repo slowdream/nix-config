843d33e76d693b60a3358087f54b1a8a61a0c17f
A.Gamov <a.gamov@a3f.ru>
2026-04-10 00:32:04 +0300

    fix

--- hosts/vm-test/hardware-configuration.nix
+++ hosts/vm-test/hardware-configuration.nix
@@ -20,7 +20,13 @@
   boot.loader.efi.canTouchEfiVariables = false;
   # при другой разметке — /boot или /boot/efi
   boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    device = "nodev";
+  };
+  boot.loader.systemd-boot.enable = false;
 
   # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/top-level/linux-kernels.nix
   boot.kernelPackages = pkgs.linuxPackages_6_18; # 6.19 плохо с драйвером NVIDIA
